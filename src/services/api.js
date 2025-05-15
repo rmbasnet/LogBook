cfd41f1a2f81e58340e7e655222b88b190e888fc
rmbasnet <basnetriam123@gmail.com>
2025-05-15 12:24:07 -0400

    final commit?

--- src/services/api.js
+++ src/services/api.js
@@ -1,19 +1,17 @@
 //js for api calls
 //seprate files for api calls
-
-require('dotenv').config();
-const API_KEY = process.env.API_KEY;
+const API_KEY = "f616e3104da7312f90a4367f9846a44f";
 
 const BASE_URL = "https://api.themoviedb.org/3"
 
 
-export const getPopularMovies = async () =>{
+export const getPopularMovies = async () => {
     const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
     const data = await response.json()
     return data.results
 };
 
-export const searchMovies = async (query) =>{
+export const searchMovies = async (query) => {
     const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`)
     const data = await response.json()
     return data.results
